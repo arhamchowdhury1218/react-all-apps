5f3226681c4a3df98ad34fb1858b50b312613a07
ärhamchowdhury1218 <arhamchowdhury1218@gmail.com>
2026-03-18 01:00:59 +0600

    project set up done

---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Root from "./components/Root/Root.jsx";
+import Home from "./components/Home/Home.jsx";
+import Apps from "./components/Apps/Apps.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/apps",
+        element: <Apps></Apps>,
+      },
+      {
+        path: "/installation-apps",
+        element: <Apps></Apps>,
+      },
+    ],
+  },
+]);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />,
+  </StrictMode>,
+);
