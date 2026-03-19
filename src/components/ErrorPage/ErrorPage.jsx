20d74cf9a4e2b28510fc10a08beaf920a74bc25c
ärhamchowdhury1218 <arhamchowdhury1218@gmail.com>
2026-03-19 13:36:58 +0600

    fixed the error page responsiveness

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -4,22 +4,23 @@ import { Link } from "react-router";
 
 const ErrorPage = () => {
   return (
-    <div className="p-20 flex flex-col lg:flex-row justify-center items-center gap-10">
+    <div className="p-20 flex flex-col justify-center items-center gap-10">
       <div className="">
         <img className="h-100 w-100" src={error404} alt="404-Scarecrow" />
       </div>
-      <div className="space-y-5 max-w-full mx-auto">
-        <h2 className="text-3xl font-bold">Opps!!</h2>
-        <p className="text-3xl font-bold">
+      <div className="space-y-5 max-w-full mx-auto text-center">
+        <h2 className="text-3xl font-bold">Oops, page not found!</h2>
+        <p className="text-2xl text-gray-500">
           The page you are looking for might be removed or is temporarily
           unavailable
         </p>
-        <Link
-          to="/"
-          className="btn bg-linear-to-r from-[#632EE3] to-[#9F62F2] flex items-center text-white font-medium w-40"
-        >
-          Back to Home Page
-        </Link>
+        <div className="text-center">
+          <Link to="/">
+            <button className="btn bg-linear-to-r from-[#632EE3] to-[#9F62F2] text-white font-medium">
+              Back to Home Page
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
